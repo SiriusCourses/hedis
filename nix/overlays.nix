0283e3229a6d4b1e1b2fb03681b3116f78dcc885
Alexander Vershilov <alexander.vershilov@gmail.com>
2020-12-03 15:50:22 +0300

    [ADM-355] Add more dependencies and cabal.project

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -44,11 +44,32 @@ with { fetch = import ./fetch.nix; };
             prometheus-client         = (fetch "prometheus-haskell") + "/prometheus-client";
             prometheus-client-extra   = (fetch "prometheus-haskell") + "/prometheus-client-extra";
             prometheus-metrics-ghc    = (fetch "prometheus-haskell") + "/prometheus-metrics-ghc";
-            cheops-db = (super.fetchzipSirius {
+            # Packages from cheops project. We should eventually put them to the separate repo or even opensource:
+            cheops-logger = (super.fetchzipSirius {
               name = "archive.zip";
               url = "https://gitlab.sirius.online/api/v4/projects/12/repository/archive.zip?sha=11e7004421a8b91569af14723a9f4c9fc353d776&s=.zip";
               sha256 = "sha256:0g8jzisgpxywfam3id5lwzzyjqgpkvi3dcxsyy3wlj8x51xmwgnd";
               }) + "/backend/cheops-logger";
+            naming-conventions = (super.fetchzipSirius {
+              name = "archive.zip";
+              url = "https://gitlab.sirius.online/api/v4/projects/12/repository/archive.zip?sha=11e7004421a8b91569af14723a9f4c9fc353d776&s=.zip";
+              sha256 = "sha256:0g8jzisgpxywfam3id5lwzzyjqgpkvi3dcxsyy3wlj8x51xmwgnd";
+              }) + "/backend/naming-conventions";
+            extended-clock = (super.fetchzipSirius {
+              name = "archive.zip";
+              url = "https://gitlab.sirius.online/api/v4/projects/12/repository/archive.zip?sha=11e7004421a8b91569af14723a9f4c9fc353d776&s=.zip";
+              sha256 = "sha256:0g8jzisgpxywfam3id5lwzzyjqgpkvi3dcxsyy3wlj8x51xmwgnd";
+              }) + "/backend/extended-clock";
+            sirius-environment = (super.fetchzipSirius {
+              name = "archive.zip";
+              url = "https://gitlab.sirius.online/api/v4/projects/12/repository/archive.zip?sha=11e7004421a8b91569af14723a9f4c9fc353d776&s=.zip";
+              sha256 = "sha256:0g8jzisgpxywfam3id5lwzzyjqgpkvi3dcxsyy3wlj8x51xmwgnd";
+              }) + "/backend/sirius-environment";
+            cheops-db = (super.fetchzipSirius {
+              name = "archive.zip";
+              url = "https://gitlab.sirius.online/api/v4/projects/12/repository/archive.zip?sha=11e7004421a8b91569af14723a9f4c9fc353d776&s=.zip";
+              sha256 = "sha256:0g8jzisgpxywfam3id5lwzzyjqgpkvi3dcxsyy3wlj8x51xmwgnd";
+              }) + "/backend/cheops-db";
          }
         );
     }
